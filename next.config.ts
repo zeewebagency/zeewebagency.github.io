e2ddfb19eb63a8ba005711fc1d9759157da32335
Uzair Arain <142334690+UzairArain2008@users.noreply.github.com>
2026-05-31 13:45:35 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/zeewebagency.github.io/",
+  assetPrefix: "/zeewebagency.github.io/",
 };
 
 export default nextConfig;
